57cce51dc927afffdd8de077f08db277d916b487
Ashley Archibald <aarchibald20@gmail.com>
2019-02-19 01:22:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,20 +4,13 @@ def reformat_languages(languages)
 
   languages.each do |k,v|
     v.each do |x, y|
-      y.each do |key, value|
-      if x == :javascript
-        new_hash[x] = {
-          :type => value,
-          :style => [:oo, :functional]
-        }
-      else
-        new_hash[x] = {
-          :type => value,
-          :style => [k]
-        }
+      if new_hash.has_key?(x)
+        new_hash[x][:style] << style
+      else 
+        new_hash[x] = type
+        new_hash[x][:style] = style
       end
     end
-  end
 end
-
+new_hash
 end
