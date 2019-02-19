87081a0a655cfdd1326b54777bd14baadd9c00ce
Ashley Archibald <aarchibald20@gmail.com>
2019-02-19 01:15:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,24 @@
 require 'pry'
 def reformat_languages(languages)
   new_hash = {}
+
+  languagues.each do |k,v|
+    language_hash.each do |x, y|
+      attributes.each do |key, value|
+      if x == :javascript
+        new_hash[x] = {
+          :type => value,
+          :style => [:oo, :functional]
+        }
+      else 
+        new_hash[language] = {
+          :type => value,
+          :style => [k]
+        }
+      end
+    end
+  end
 end
+return new_hash
+end
+
